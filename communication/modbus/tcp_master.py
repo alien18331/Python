eec28d71a39cdc757364eab8fdb09927cbc1143b
alien18331 <alien18331@gmail.com>
2019-12-19 10:45:42 +0800

    update modbus

--- communication/modbus/tcp_master.py
+++ communication/modbus/tcp_master.py
@@ -1,3 +1,4 @@
+
 import sys
 import time
 import logging
@@ -6,18 +7,23 @@ import modbus_tk.defines as cst
 import modbus_tk.modbus_tcp as modbus_tcp
 
 logger = modbus_tk.utils.create_logger("console")
+
+_IPAddress = "127.0.0.1"
+_Slave = 1
+_DataLen = 8
+
+
 if __name__ == "__main__":
     try:
         # 连接MODBUS TCP从机
-        master = modbus_tcp.TcpMaster(host="127.0.0.1")
+        master = modbus_tcp.TcpMaster(host = _IPAddress)
         master.set_timeout(5.0)
         logger.info("connected")
         
         while True:
             # 读保持寄存器
-            logger.info(master.execute(1, cst.READ_HOLDING_REGISTERS, 0, 8))
+            logger.info(master.execute(_Slave, cst.READ_HOLDING_REGISTERS, 0, _DataLen))
             time.sleep(1)
-            
             
     except modbus_tk.modbus.ModbusError as e:
         logger.error("%s- Code=%d" % (e, e.get_exception_code()))
--- communication/modbus/tcp_slave.py
+++ communication/modbus/tcp_slave.py
@@ -1,27 +1,43 @@
+
+import time
+import random
+import logging
 import modbus_tk
-import modbus_tk.modbus as modbus
 import modbus_tk.defines as cst
+import modbus_tk.modbus as modbus
 import modbus_tk.modbus_tcp as modbus_tcp
 
+logger = modbus_tk.utils.create_logger(name = "console", record_format = ">> %(message)s")
 
-#Create the server
-            
+# Create server
+server = modbus_tcp.TcpServer(address = "127.0.0.1", port = 502) # Default port = 502
+slaver = server.add_slave(1) # Slave_ID = 1
 
-try:
-      # parameter(ip/port)
-      
-      server = modbus_tcp.TcpServer(address=_localip,port=_port)
-      ##logger.info("enter 'quit' for closing the server")			      
-      server.start()
-      print ('server start..')
-      slave = server.add_slave(_slaveid)
-      slave.add_block('ro', cst.HOLDING_REGISTERS, 0, 100)
-except: 
-      print('server create fail')
-      server.stop()
-      server._do_exit()
-      
-      
-# parameter (id,address,data)
-slave.set_values('ro', 0 , data)
-
+def setup():
+    slaver.add_block("A", cst.HOLDING_REGISTERS, 0, 16)
+    # slaver.set_values("A", 0, 0)
+       
+def loop():
+    logger.info("running...")
+    
+    # START
+    server.start()    
+    while True:
+        # SET VALUE
+        slaver.set_values("A", 0, random.randint(0,100))
+        slaver.set_values("A", 1, random.randint(0,100))
+        
+        # DELAY
+        time.sleep(1)
+        
+def destory():
+    logger.info("destory")
+    # STOP
+    server.stop()
+       
+if __name__ == "__main__":
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:
+        destory()
