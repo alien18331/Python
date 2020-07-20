2eba3467b9d8b14831e521c37ccb63d670c1b3d5
alien18331 <alien18331@gmail.com>
2020-07-20 14:47:53 +0800

    update gpio

---
+++ Function/GPIO/GPIO_output.py
@@ -0,0 +1,18 @@
+#!/usr/bin/ python3
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(21,GPIO.OUT)
+
+while True:
+  try:
+    GPIO.output(21,True)
+    time.sleep(0.2)
+    GPIO.output(21,False)
+    time.sleep(1)
+    print("111")
+  except (KeyboardInterrupt, SystemExit):
+    GPIO.cleanup()
+    print("exit")
